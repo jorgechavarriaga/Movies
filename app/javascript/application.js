81b000427def69df24242b973b4b50036d21dca7
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2023-03-08 11:07:43 -0500

    Add raty and views for star image for rating

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require bootstrap-sprockets
-
+import "jquery"
+import "bootstrap"
 import "@hotwired/turbo-rails"
 import "controllers"
-
